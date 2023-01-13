0b0e3c0bdbf4249e76bf8637ccad7aeb2bff2a20
EnzoVenon <enzovenon@laposte.net>
2023-01-13 15:05:52 +0100

    ex 0.6

--- src/test/java/org/acme/graph/routing/DijkstraRegressTest.java
+++ src/test/java/org/acme/graph/routing/DijkstraRegressTest.java
@@ -2,7 +2,6 @@ package org.acme.graph.routing;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 
 import java.util.List;
@@ -11,6 +10,7 @@ import org.acme.graph.TestGraphFactory;
 import org.acme.graph.errors.NotFoundException;
 import org.acme.graph.model.Edge;
 import org.acme.graph.model.Graph;
+import org.acme.graph.model.Path;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,9 +34,9 @@ public class DijkstraRegressTest {
 
 	@Test
 	public void testABFound() {
-		List<Edge> path = finder.findPath(graph.findVertex("a"), graph.findVertex("b"));
+		Path path = finder.findPath(graph.findVertex("a"), graph.findVertex("b"));
 		assertNotNull(path);
-		assertEquals(1, path.size());
+		assertEquals(1, path.getEdges().size());
 	}
 
 	@Test
@@ -48,18 +48,19 @@ public class DijkstraRegressTest {
 
 	@Test
 	public void testACFoundWithCorrectOrder() {
-		List<Edge> path = finder.findPath(graph.findVertex("a"), graph.findVertex("c"));
+		Path path = finder.findPath(graph.findVertex("a"), graph.findVertex("c"));
 		assertNotNull(path);
-		assertEquals(2, path.size());
+		List<Edge> edges = path.getEdges();
+		assertEquals(2, edges.size());
 
 		int index = 0;
 		{
-			Edge edge = path.get(index++);
+			Edge edge = edges.get(index++);
 			assertEquals("a", edge.getSource().getId());
 			assertEquals("b", edge.getTarget().getId());
 		}
 		{
-			Edge edge = path.get(index++);
+			Edge edge = edges.get(index++);
 			assertEquals("b", edge.getSource().getId());
 			assertEquals("c", edge.getTarget().getId());
 		}
